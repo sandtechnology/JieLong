a2c3644b9dd38d8ae53bdfffcdc7cf9a472fca06
SeraphJACK <seraphjack@outlook.com>
2019-08-09 23:45:38 +0800

    Fix typo

--- src/main/java/top/seraphjack/jielong/JieLong.java
+++ src/main/java/top/seraphjack/jielong/JieLong.java
@@ -8,9 +8,9 @@ import top.seraphjack.jielong.command.CommandJieLong;
 import top.seraphjack.jielong.idiom.IdiomProvider;
 import top.seraphjack.jielong.idiom.IdiomProviderFile;
 
-@Mod(modid = JieLong.MODID, name = JieLong.NAME, version = JieLong.VERSION, serverSideOnly = true)
+@Mod(modid = JieLong.MODID, name = JieLong.NAME, version = JieLong.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
 public class JieLong {
-    public static final String MODID = "jielong", NAME = "JieLong", VERSION = "@VERSION_INJECT";
+    public static final String MODID = "jielong", NAME = "JieLong", VERSION = "@VERSION_INJECT@";
 
     public static final Logger logger = LogManager.getLogger(MODID);
 
